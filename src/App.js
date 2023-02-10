f513463d75bb35096b5daf3a667e0aee56e6cadd
kaliloaida <kaliloaida@gmail.com>
2023-02-10 15:37:11 +0600

    Create EditingListing page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
 
 function App() {
   return (
@@ -25,8 +26,11 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/create-listing" element={<PrivateRoute/>}>
+          <Route path="/create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="/edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
           </Route>
         </Routes>
       </Router>
